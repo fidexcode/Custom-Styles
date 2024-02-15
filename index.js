97833e1f467e22a30170730b6218a8c1b848bdca
fidexcode <fidexcode@gmail.com>
2024-02-15 08:46:37 +0100

    made use of arrow function

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const bgColorInput = document.getElementById("bg-color");
 const textColorInput = document.getElementById("text-color");
 
 // Update the CSS variables when the inputs change
-bgColorInput.addEventListener("input", function () {
+bgColorInput.addEventListener("input", () => {
   document.documentElement.style.setProperty("--bgColor", bgColorInput.value);
 });
 
-textColorInput.addEventListener("input", function () {
+textColorInput.addEventListener("input", () => {
   document.documentElement.style.setProperty(
     "--textColor",
     textColorInput.value,
